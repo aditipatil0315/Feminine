889629ae0ec354c247dea654f3f9610a3fc06d52
Aditi Patil <aditiprakashpatil7@gmail.com>
2024-09-24 17:19:09 +0530

    products page landing page done

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,50 @@
+import maternityImage from '../../assets/img1.png.png';  // adjust the path based on your folder structure
+import periodImage from '../../assets/workout.png';
+import fertilityImage from '../../assets/img1.png.png';
+import postpartumImage from '../../assets/workout.png';
+import exerciseImage from '../../assets/img1.png.png';
+import pelvicHealthImage from '../../assets/workout.png';
+import menopauseImage from '../../assets/img1.png.png';
+import vaginalHealthImage from '../../assets/workout.png';
+import mentalHealthImage from '../../assets/img1.png.png';
+
+const productsArray = [
+    {
+        productName: "Maternity Products" ,
+        imgURL : maternityImage
+    },
+    {
+        productName: "Period Products" ,
+        imgURL : periodImage
+    },
+    {
+        productName: "Fertility & Pregnancy Health" ,
+        imgURL : fertilityImage
+    },
+    {
+        productName: "Postpartum Care" ,
+        imgURL : postpartumImage
+    },
+    {
+        productName: "Exercise & Fitness for Women" ,
+        imgURL : exerciseImage
+    },
+    {
+        productName: "Pelvic Health" ,
+        imgURL : pelvicHealthImage
+    },
+    {
+        productName: "Menopause Care" ,
+        imgURL : menopauseImage
+    },
+    {
+        productName: " Vaginal Health" ,
+        imgURL : vaginalHealthImage
+    },
+    {
+        productName: "Mental Health & Relaxation" ,
+        imgURL : mentalHealthImage
+    },
+]
+
+export default productsArray;
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import productsArray from "./Products"
+import './Products.css'
 
 const Products = () => {
   return (
-    <div>Products</div>
+      <div className="products_container">
+
+                <h3>Select the category of the products below -</h3> <br /><br />
+        < div className="product_cards">
+
+          {productsArray.map((item, idx) => (
+            <div key={idx} className="product">
+
+            <h3 >{item.productName}</h3>
+            <img src= {item.imgURL}  />
+            
+            </div>
+          ))}
+          
+          
+        </div>
+
+      </div>
   )
 }
 
